ac723cdc1c64ca22a3aecde2ba58b6db9d726eed
Nik Kor <3357381Nk@gmail.com>
2024-08-09 15:26:31 +0100

    basic improvements

--- fill_data.py
+++ fill_data.py
@@ -1,8 +1,15 @@
 import faker
 import sqlite3
-from random import randint, choice
+from random import randint
 
 students_number = randint(30, 50)
 groups_number = 3
 tutors_number = randint(3, 5)
 subjects_number = randint(5, 8)
+
+def generate_students():
+    fake = faker.Faker()
+    students = []
+    for _ in range(students_number):
+        students.append((fake.first_name(), fake.last_name(), randint(1, groups_number)))
+    return students
--- groups.sql
+++ groups.sql
@@ -4,49 +4,55 @@
 --3. tutors (ids and names)
 --4. subjects (ids, names and tutors ids)
 --5. marks (field id, students ids, subjects ids and marks)
-
 -- Creating table students
 DROP TABLE IF EXISTS students;
+
 CREATE TABLE
     students (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name VARCHAR(100) NOT NULL
+        student_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(100) NOT NULL,
+        group_id INTEGER,
+        FOREIGN KEY (group_id) REFERENCES groups (group_id) ON DELETE CASCADE ON UPDATE CASCADE
     );
 
 -- Creating table groups
 DROP TABLE IF EXISTS groups;
+
 CREATE TABLE
     groups (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        group_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name VARCHAR(100) NOT NULL
     );
 
 -- Creating table tutors
 DROP TABLE IF EXISTS tutors;
+
 CREATE TABLE
     tutors (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tutor_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name VARCHAR(100) NOT NULL
     );
 
 -- Creating table subjects
 DROP TABLE IF EXISTS subjects;
+
 CREATE TABLE
     subjects (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        subject_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name VARCHAR(100) NOT NULL,
         tutor_id INTEGER,
-        FOREIGN KEY (tutor_id) REFERENCES tutors (id) ON DELETE CASCADE ON UPDATE CASCADE
+        FOREIGN KEY (tutor_id) REFERENCES tutors (tutor_id) ON DELETE CASCADE ON UPDATE CASCADE
     );
 
 -- Creating table marks
 DROP TABLE IF EXISTS marks;
+
 CREATE TABLE
     marks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        mark_id INTEGER PRIMARY KEY AUTOINCREMENT,
         student_id INTEGER,
         subject_id INTEGER,
         mark TINYINT,
-        FOREIGN KEY (student_id) REFERENCES students (id) ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY (subject_id) REFERENCES subjects (id) ON DELETE CASCADE ON UPDATE CASCADE
+        FOREIGN KEY (student_id) REFERENCES students (student_id) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (subject_id) REFERENCES subjects (subject_id) ON DELETE CASCADE ON UPDATE CASCADE
     );
